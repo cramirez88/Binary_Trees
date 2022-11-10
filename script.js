d7e32a64eb1bbd540a01962cf090ad4384368daf
Christian Ramirez <christian.r0602@gmail.com>
2022-11-09 19:34:45 -0600

    breadth first

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-const depthFirstValues = (root) => {
+// const depthFirstValues = (root) => {
+//   if(!root) return []
+//   let stack = [ root ]
+//   let newArr = []
+//   while(stack.length > 0){
+//     let node = stack.pop()
+//     newArr.push(node.val)
+//     if(node.right !== null) stack.push(node.right)
+//     if(node.left !== null) stack.push(node.left)
+//   }
+//   return newArr
+// };
+
+const breadthFirstValues = (root) => {
   if(!root) return []
-  let stack = [ root ]
-  let newArr = []
-  while(stack.length > 0){
-    let node = stack.pop()
+  let queue = [root]
+  let newArr =[]
+  while(queue.length > 0){
+    let node = queue.shift()
     newArr.push(node.val)
-    if(node.right !== null) stack.push(node.right)
-    if(node.left !== null) stack.push(node.left)
+    
+    if(node.left !== null) queue.push(node.left)
+    if(node.right !== null) queue.push(node.right)
   }
   return newArr
 };
