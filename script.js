97f1de22c1b58768fde01973ed4d7b78a952fced
Christian Ramirez <christian.r0602@gmail.com>
2022-11-14 21:21:49 -0600

    how high recursive

--- script.js
+++ script.js
@@ -157,6 +157,13 @@ const treeValueCount = (root, target) => {
 };
 
 
+const howHigh = (node) => {
+  if(!node) return -1
+  return 1 + Math.max(howHigh(node.left), howHigh(node.right))
+};
+
+
+
 
 
 
